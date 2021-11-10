df1d225474b15f0e7ee27e01afecd46d8b2e3111
Alex McConnell <alexmmcconnell@gmail.com>
2021-11-10 11:21:37 -0700

    Refactor - Adjust controller names

--- app/controllers/plot_plants_controller.rb
+++ app/controllers/plot_plants_controller.rb
@@ -1,4 +1,4 @@
-class PlotPlantController < ApplicationController
+class PlotPlantsController < ApplicationController
   def delete
     plot_plant = PlotPlant.find_by(plant_id: params[:plant], plot_id: params[:plot])
     plot_plant.destroy
