df8f14670bc8fd0161f091f88b5093487c232e21
Alex McConnell <alexmmcconnell@gmail.com>
2021-11-10 10:48:33 -0700

    Feat - Add ability to remove plants from plots

---
+++ app/controllers/plot_plant_controller.rb
@@ -0,0 +1,7 @@
+class PlotPlantController < ApplicationController
+  def delete
+    plot_plant = PlotPlant.find_by(plant_id: params[:plant], plot_id: params[:plot])
+    plot_plant.destroy
+    redirect_to "/plots"
+  end
+end
